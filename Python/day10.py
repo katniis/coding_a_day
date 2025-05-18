4b922afa4e27ba6b3507e42e265917d4819e31bc
katniss <katnis00001@gmail.com>
2025-05-18 09:48:21 +0800

    fixed day 10 not in py folder

---
+++ Python/day10.py
@@ -0,0 +1,12 @@
+# 10. split string using a delimiter
+
+# split()
+
+email_address: str = "juan.delacruz@gmail.com"
+
+split_string = email_address.split("@")
+
+
+print(split_string[0]) # juan.delacruz
+print(split_string[1]) # gmail.com
+
