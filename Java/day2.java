75b9145b22c9bca8e7beb13972d4066251184745
katniss <katnis00001@gmail.com>
2025-09-10 08:35:28 +0800

    Add Java program to concatenate and print "Hello, World!"

---
+++ Java/day2.java
@@ -0,0 +1,10 @@
+//  2. declare 2 string variables, 1 is hello, 1 is world, concatenate to say hello world
+
+class day2{
+    public static void main(String[] args){
+        String  str1 = "Hello";
+        String str2 = "World";
+
+        System.out.println(str1 + ", " + str2 + "!");
+    }
+}
